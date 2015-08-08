77ad6324de241c5394da61deb12069d5783213a0
philos <philos99@gmail.com>
2015-08-08 20:48:35 +0900

    site 이동

---
+++ bm/api/api-search.js
@@ -0,0 +1,110 @@
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) { 
+    return typeof args[number] != 'undefined'
+      ? args[number]
+      : match
+    ;
+  });
+};
+
+var buttons = document.getElementsByTagName("button"); 
+for (var i = 0; i < buttons.length; i++)
+  buttons[i].onclick = handleClick; 
+
+var buttonInfos = 
+  // id : [url, isSpacedInput, isIframed]
+    { clojure   : ["http://clojuredocs.org/#{0}", false, false],
+      //cljs      : ["https://www.google.co.kr/search?q={0}+site:github.com/cljsinfo/api-refs/tree/catalog", false, false],
+    clhs       : ["http://www.lispworks.com/cgi-bin/search.cgi?q={0}", false, true],
+
+    w3c        : ["https://www.google.co.kr/#hl=ko&q={0}+site:www.w3schools.com",
+                  false, false],
+    webapi     : ["https://developer.mozilla.org/en-US/search?q={0}", false, false],
+    jquery     : ["http://api.jquery.com/?ns0=1&s={0}", false, true],
+
+    java       : ["https://www.google.co.kr/#hl=ko&q={0}+site:docs.oracle.com/javase/7/docs/api/",
+                  false, false],
+    javaex     : ["https://www.google.co.kr/#hl=ko&q={0}+site:www.java2s.com/Code/JavaAPI/",
+                  false, false],
+    antlr      : ["https://www.google.co.kr/#hl=ko&q={0}+site:antlr.org",
+                  false, false],
+    itext      : ["https://www.google.co.kr/#hl=ko&q={0}+site:api.itextpdf.com/itext/",
+                  false, false],
+
+      
+
+    d          : ["https://www.google.co.kr/#hl=ko&q={0}+site:dlang.org",
+                   false, false],
+
+    lua        : ["https://www.google.co.kr/#hl=ko&q={0}+site:lua-users.org/wiki/",
+                  false, false],
+    luajit     : ["https://www.google.co.kr/#hl=ko&q={0}+site:luajit.org", false, false],
+
+    context    : ["http://wiki.contextgarden.net/index.php?search={0}", false, true],
+    contextapi : ["http://wiki.contextgarden.net/Command/{0}", false, true],
+
+    emacs      : ["https://www.google.co.kr/#hl=ko&q=emacs+{0}", false, false],
+    emacswiki  : ["https://www.google.co.kr/#hl=ko&q={0}+site:www.emacswiki.org/", 
+                   false, false],
+
+
+    google     : ["https://www.google.co.kr/#hl=ko&q={0}", false, false],
+ };
+
+var oldInput = "";
+
+/*
+function handleLoad(e)
+{
+  var guide =  buttonInfos['guide'];
+  var iframe = document.getElementById("iframe");
+  iframe.src = guide[0];
+}
+*/
+
+function handleClick(e)
+{
+  var inputValue = document.getElementById("entry").value;
+  spacedInput =  inputValue;
+    plusedInput = inputValue.split(" ").join("+");
+
+
+  var value = buttonInfos[e.target.id];
+  var url = value[0].format(value[1] ? spacedInput : plusedInput);
+
+  if (value[2])   // iframe에 담길 수 있으면
+  {           
+    var iframe = document.getElementById("iframe");
+    iframe.src = url;
+  }
+  else 
+  {
+    var anchor = document.getElementById("anchor");
+    anchor.href = url;
+    var evt = document.createEvent("MouseEvents");
+    evt.initEvent("click", true, true);
+    anchor.dispatchEvent(evt);
+  }
+} 
+
+function handleFocus(e) {
+  oldInput = document.getElementById("entry").value;
+  document.getElementById("entry").value = "";
+}
+
+function handleKeydown(e) {
+  if (e.keyCode == 13) {   // 13 == Enter Key
+    var voca = document.getElementById("clojure");
+    var evt = document.createEvent("MouseEvents");
+    evt.initEvent("click", true, true);
+    voca.dispatchEvent(evt);
+  }
+  else if (e.keyCode == 38) {  // 38 == UpArrow Key
+    e.target.value = oldInput;
+  } 
+}
+
+document.getElementById("entry").onkeydown = handleKeydown;
+document.getElementById("entry").onfocus = handleFocus;
+// document.getElementById("main").onload = handleLoad;
---
+++ dic/dic-search.js
@@ -0,0 +1,152 @@
+String.prototype.format = function()
+{
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) { 
+    return typeof args[number] != 'undefined'
+      ? args[number]
+      : match
+    ;
+  });
+};
+
+function setCookie(cname, cvalue, exdays)
+{
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++)
+		{
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+$(document).ready(function() {
+
+  var buttonInfos = 
+  // id : [url, inputKind, isIframed]    inputKind : "space", "plus", "minus"
+  { guide      : ["guide.html", "space", true],
+    home       : ["http://www.english4u.kr/", "space", false],
+
+    cambridge  : ["http://dictionary.cambridge.org/search/british/?q={0}", "plus", true],
+    webster    : ["http://www.merriam-webster.com/dictionary/{0}", "space", true],
+    vocabulary : ["http://www.vocabulary.com/dictionary/{0}", "space", true],
+    yourdic    : ["http://www.yourdictionary.com/{0}", "minus", true],
+
+    etymology  : ["http://www.etymonline.com/index.php?allowed_in_frame=0"
+                  + "&search={0}&searchmode=none", "plus", false],
+    wiktionary : ["http://en.wiktionary.org/wiki/{0}", "space", true],
+    dictionary : ["http://dictionary.reference.com/browse/{0}", "plus", true],
+    free       : ["http://www.thefreedictionary.com/{0}", "plus", true],
+    thesaurus  : ["http://thesaurus.com/browse/{0}?s=t", "plus", true],
+    collins    : ["http://dictionary.reverso.net/english-cobuild/{0}", "space", true],
+    onelook    : ["http://www.onelook.com/?w={0}&ls=a", "plus", true],
+
+    naver      : ["http://endic.naver.com/search.nhn?sLn=kr&searchOption=all&query={0}",
+                  "space", false],
+    daum       : ["http://dic.daum.net/search.do?q={0}", "space", true],
+    sisa       : ["http://www.ybmallinall.com/stylev2/dicview.asp?kwdseq=0" + 
+                  "&kwdseq2=0&DictCategory=DictAll&DictNum=0&ById=0&PageSize=5&StartNum=0" +
+                  "&GroupMode=0&cmd=0&kwd={0}&x=0&y=0", "plus", true],
+    bluedic    : ["http://www.bluedic.com/{0}", "plus", true],
+    impact     : ["http://dic.impact.pe.kr/ecmaster-cgi/search.cgi?bool=and&word=yes&kwd={0}",
+                  "plus", true],
+
+    idiom      : ["https://www.google.co.kr/#hl=ko&q={0}+~meaning+OR+~definition", "plus", false],
+    origin     : ["https://www.google.co.kr/#hl=ko&q={0}+origin+OR+root+OR+etymology",
+                  "plus", false],
+    colloc     : ["http://www.ozdic.com/collocation-dictionary/{0}", "space", true],
+    slang      : ["http://www.urbandictionary.com/define.php?term={0}", "plus", false],
+    ngram      : ["http://books.google.com/ngrams/graph?content={0}&year_start=1500"
+                  + "&year_end=2000&corpus=0&smoothing=0", "plus", true],
+    regex      : ["http://www.visca.com/regexdict/", "plus", true],
+
+    google     : ["https://www.google.co.kr/#hl=ko&q={0}", "plus", false],
+   };
+
+  function handleClick(e)
+  {
+    var inputValue = $("#entry").val();
+    var spaceInput =  inputValue;
+    var plusInput = inputValue.split(" ").join("+");
+    var minusInput = inputValue.split(" ").join("-");
+    var input;
+
+    var value = buttonInfos[e.target.id];
+    if (value[1] == "space")
+      input = spaceInput;
+    else if (value[1] == "plus")
+      input = plusInput;
+    else
+      input = minusInput;
+
+    var url = value[0].format(input);
+
+    setCookie('search-words', inputValue, 1);
+
+    if (value[2])   // iframe에 담길 수 있으면
+    {
+      $("#iframe")[0].src = url;
+    }
+    else 
+    {
+      window.location.href = url;
+    }
+		$("#entry").focus();
+  } 
+
+  function handleFocus(e)
+	{
+    $(this).val("");
+  }
+
+  function handleKeydown(e)
+  {
+    if (e.keyCode == 13)   // 13 == Enter Key
+		{
+      setCookie('search-word', $(this).val(), 1);
+      $("#cambridge").click();
+    }
+    else if (e.keyCode == 38)  // 38 == UpArrow Key
+		{
+      $(this).val( getCookie('search-words') );
+    }
+		else
+		{
+			if ( $(this).val() == getCookie('search-words') )
+			{
+				  $(this).val("");
+		  }
+		}
+  }
+
+
+  function handleChange(e)
+	{
+		 if ( $(this).val() == getCookie('search-words') )
+				  $(this).val(e.keyCode);
+	}
+
+	function handleLoad(e)
+	{
+		 $("#entry").focus();
+		 $("#entry").val( getCookie('search-words') );
+	}
+
+  $("button").click(handleClick);
+  $("#entry").keydown(handleKeydown);
+//  $("#entry").focusin(handleFocus);
+	//$("#ettry").change(handleChange); 
+
+  window.onload = handleLoad;	
+});
+
